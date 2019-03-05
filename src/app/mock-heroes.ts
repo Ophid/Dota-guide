20a2e8fdcbefb5fe9d73f0cbb2d0c81cb38ce75a
Ophid <gaurabdutta89@gmail.com>
2019-03-06 04:08:11 +0530

    First version

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Sven' },
+  { id: 12, name: 'Windranger' },
+  { id: 13, name: 'Necrophos' },
+  { id: 14, name: 'Pudge' },
+  { id: 15, name: 'Alchemist' },
+  { id: 16, name: 'Broodmother' },
+  { id: 17, name: 'ShadowFiend' },
+  { id: 18, name: 'Luna' },
+  { id: 19, name: 'Rylai' },
+  { id: 20, name: 'Viper' }
+];
