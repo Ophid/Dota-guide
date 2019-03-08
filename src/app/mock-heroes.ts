a32ce4437ea85550f552dce645d92aa7155bce53
Ophid <gaurabdutta89@gmail.com>
2019-03-08 19:26:07 +0530

    second

--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -1,14 +1,14 @@
 import { Hero } from './hero';
 
 export const HEROES: Hero[] = [
-  { id: 11, name: 'Sven' },
-  { id: 12, name: 'Windranger' },
-  { id: 13, name: 'Necrophos' },
-  { id: 14, name: 'Pudge' },
-  { id: 15, name: 'Alchemist' },
-  { id: 16, name: 'Broodmother' },
-  { id: 17, name: 'ShadowFiend' },
-  { id: 18, name: 'Luna' },
-  { id: 19, name: 'Rylai' },
-  { id: 20, name: 'Viper' }
+  { id: 1, name: 'Sven' },
+  { id: 2, name: 'Windranger' },
+  { id: 3, name: 'Necrophos' },
+  { id: 4, name: 'Pudge' },
+  { id: 5, name: 'Alchemist' },
+  { id: 6, name: 'Broodmother' },
+  { id: 7, name: 'ShadowFiend' },
+  { id: 8, name: 'Luna' },
+  { id: 9, name: 'Rylai' },
+  { id: 10, name: 'Viper' }
 ];
